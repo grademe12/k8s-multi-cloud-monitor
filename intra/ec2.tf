a080544166aaf55f6a19bb6e44fcdec04897e96b
woosupar <grademe12@gmail.com>
2025-11-19 12:32:08 +0900

    fix: ec2.tf

--- intra/ec2.tf
+++ intra/ec2.tf
@@ -19,7 +19,9 @@ resource "aws_instance" "k3s_master" {
   private_ip             = "10.1.2.10"
   iam_instance_profile = aws_iam_instance_profile.k8s_node.name
 
-  user_data = base64encode(templatefile("${path.module}/bootstrap.yml", {}))
+  user_data = base64encode(templatefile("${path.module}/bootstrap.yml", {
+  ansible_pub_key = file("${path.module}/keys/ansible_rsa.pub")
+  }))
   user_data_replace_on_change = true
 
   root_block_device {
@@ -42,7 +44,9 @@ resource "aws_instance" "k3s_worker" {
   key_name               = aws_key_pair.main.key_name
   iam_instance_profile = aws_iam_instance_profile.k8s_node.name
 
-  user_data = base64encode(templatefile("${path.module}/bootstrap.yml", {}))
+  user_data = base64encode(templatefile("${path.module}/bootstrap.yml", {
+  ansible_pub_key = file("${path.module}/keys/ansible_rsa.pub")
+  }))
   user_data_replace_on_change = true
 
   root_block_device {
@@ -67,7 +71,9 @@ resource "aws_instance" "postgresql" {
   key_name               = aws_key_pair.main.key_name
   private_ip             = "10.1.2.100"
 
-  user_data = base64encode(templatefile("${path.module}/bootstrap.yml", {}))
+  user_data = base64encode(templatefile("${path.module}/bootstrap.yml", {
+  ansible_pub_key = file("${path.module}/keys/ansible_rsa.pub")
+  }))
   user_data_replace_on_change = true
 
   root_block_device {
